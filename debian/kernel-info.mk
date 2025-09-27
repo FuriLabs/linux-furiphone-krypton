1a2a619ed8315bf2f9fef1d1aad50c2c7da09dbc
Bardia Moshiri <bardia@furilabs.com>
2025-09-26 22:41:31 -0400

    debian: bump to 4.19.325
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -8,7 +8,7 @@
 VARIANT = android
 
 # Kernel base version
-KERNEL_BASE_VERSION = 4.19.191
+KERNEL_BASE_VERSION = 4.19.325
 
 # The kernel cmdline to use
 KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2
