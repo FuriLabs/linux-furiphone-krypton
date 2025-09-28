df1b0baad079969eb38433f245b6fd895700ff99
Bardia Moshiri <bardia@furilabs.com>
2025-09-28 02:12:16 -0400

    v4l2-core: drop unused variable
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- drivers/media/v4l2-core/v4l2-mem2mem.c
+++ drivers/media/v4l2-core/v4l2-mem2mem.c
@@ -489,7 +489,6 @@ int v4l2_m2m_querybuf(struct file *file, struct v4l2_m2m_ctx *m2m_ctx,
 {
 	struct vb2_queue *vq;
 	int ret = 0;
-	unsigned int i;
 
 	vq = v4l2_m2m_get_vq(m2m_ctx, buf->type);
 	ret = vb2_querybuf(vq, buf);
